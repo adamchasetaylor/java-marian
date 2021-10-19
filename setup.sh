3b66a1901e6c5cb59d83797ded79e4385f312727
Adam Taylor <adtaylor@twilio.com>
2021-10-19 12:11:26 -0500

    updated setup with java compile, java run

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Setup the Structure
 mkdir -p src/main/java
- 
+
+# Re-Install Dependencies from POM.XML. Store them locally.
 mvn clean install dependency:copy-dependencies 
+
+# Run it locally
+javac -classpath .:target/dependency/* -d . src/main/java/Hello.java
+java -classpath .:target/dependency/* Hello
